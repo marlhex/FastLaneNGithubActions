8f17c577711201fb2d4b17f020c6632e4b4e1f7f
Ignacio Arias <1992ignacio@gmail.com>
2024-01-13 19:38:08 -0800

    Welcome

--- FastLaneNGithubActions/ContentView.swift
+++ FastLaneNGithubActions/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, this is Marlhex!")
+            Text("Welcome")
         }
         .padding()
     }
