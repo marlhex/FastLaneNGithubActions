cc3f22fc9468acaf9574a3b6b9a2bed08d279249
Ignacio Arias <1992ignacio@gmail.com>
2024-01-13 19:31:29 -0800

    Initial Commit

---
+++ FastLaneNGithubActions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FastLaneNGithubActions
+//
+//  Created by Ignacio Arias on 2024-01-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, this is Marlhex!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FastLaneNGithubActions/FastLaneNGithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastLaneNGithubActionsApp.swift
+//  FastLaneNGithubActions
+//
+//  Created by Ignacio Arias on 2024-01-13.
+//
+
+import SwiftUI
+
+@main
+struct FastLaneNGithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
